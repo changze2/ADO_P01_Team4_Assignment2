5ba5da50ee513c94fa3e1e247dc58e35680e8a9c
ElvisChanJunYu <elvischan031@gmail.com>
2025-01-03 20:50:53 +0800

    dbt for f1 data

---
+++ models/transform/trans_drivers_api.sql
@@ -0,0 +1,15 @@
+--Elvis: Decided to drop first_name / last_name / headshot_url since full_name is already created and headshot_url cannot provide valueable insights
+SELECT
+    COALESCE(broadcast_name, '') AS broadcast_name,
+    COALESCE(country_code, NULL) AS country_code,
+    COALESCE(driver_number, 0) AS driver_number,
+    COALESCE(full_name, '') AS full_name,
+    COALESCE(meeting_key, '') AS meeting_key,
+    COALESCE(name_acronym, '') AS name_acronym,
+    COALESCE(session_key, '') AS session_key,
+    COALESCE(team_colour, '') AS team_colour,
+    COALESCE(team_name, '') AS team_name
+FROM {{ ref('stg_drivers_api') }}
+{% if is_incremental() %}
+    {{ incremental_session_meeting_key() }}
+{% endif %}
---
+++ models/transform/trans_weather_api.sql
@@ -0,0 +1,39 @@
+
+
+SELECT
+    AIR_TEMPERATURE,
+    EXTRACT(YEAR FROM DATE) AS YEAR,
+    EXTRACT(MONTH FROM DATE) AS MONTH,
+    EXTRACT(DAY FROM DATE) AS DAY,
+    HUMIDITY,
+    MEETING_KEY,
+    SESSION_KEY,
+    PRESSURE,
+    RAINFALL,
+    TRACK_TEMPERATURE,
+    WIND_DIRECTION,
+    WIND_SPEED,
+-- Handling missing values: Fill missing AIR_TEMPERATURE with the average value
+    COALESCE(AIR_TEMPERATURE, (
+        SELECT AVG(AIR_TEMPERATURE) 
+        FROM {{ ref('stg_weather_api') }}
+    )) AS AIR_TEMPERATURE_FILLED,
+    
+    -- Create derived features, e.g., Heat Index or Wind Chill
+    CASE 
+        WHEN AIR_TEMPERATURE IS NOT NULL AND HUMIDITY IS NOT NULL THEN
+            AIR_TEMPERATURE + (0.55 * (1 - HUMIDITY / 100) * (AIR_TEMPERATURE - 58))
+        ELSE NULL
+    END AS HEAT_INDEX,
+    
+    CASE
+        WHEN AIR_TEMPERATURE IS NOT NULL AND WIND_SPEED IS NOT NULL THEN
+            AIR_TEMPERATURE - (0.7 * WIND_SPEED) -- Wind Chill simplified formula
+        ELSE NULL
+    END AS WIND_CHILL
+    
+FROM {{ ref('stg_weather_api') }}
+
+{% if is_incremental() %}
+    {{ incremental_session_meeting_key() }}
+{% endif %}
