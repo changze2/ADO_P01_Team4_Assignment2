e6db803af321aa5058e1402dfa723fff47246a09
namasayaelvis <151112132+ElvisChanJunYu@users.noreply.github.com>
2025-02-02 11:46:50 +0800

    Delete dags/testing2 directory

--- dags/testing2/models/staging/stg_races.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-*
-FROM
-    {{ source('aston_martin_data', 'RACES') }}
--- dags/testing2/models/transform/transf_races.sql
+++
@@ -1,5 +0,0 @@
-with renamed_races as (
-    {{ rename_circuit_id('stg_races') }}  -- Apply the macro on the transform model
-)
-
-select * from renamed_races
