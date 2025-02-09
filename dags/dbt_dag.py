ecfe1e908850f59cdb6b0b52c1a896cb9ee7602a
namasayaelvis <151112132+ElvisChanJunYu@users.noreply.github.com>
2025-02-10 03:36:43 +0800

    Update dbt_dag.py

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -1,11 +1,10 @@
 import os
 from datetime import datetime
-from airflow import DAG
-from airflow.operators.bash import BashOperator
+
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 
-# Profile configuration for Snowflake
+
 profile_config = ProfileConfig(
     profile_name="default",
     target_name="dev",
@@ -15,40 +14,13 @@ profile_config = ProfileConfig(
     )
 )
 
-# Define the Airflow DAG
-dag = DAG(
-    "dbt_dag_with_git_pull",
-    default_args={
-        "owner": "airflow",
-        "start_date": datetime(2024, 12, 25),
-    },
-    schedule_interval="0 1 * * *",
-    catchup=False,
-)
-
-# Task to pull the latest changes from the main-dbt branch
-pull_dbt_branch = BashOperator(
-    task_id="pull_dbt_branch",
-    bash_command="""
-    cd /usr/local/airflow/dags/dbt_testing &&
-    git fetch origin main-dbt &&
-    git checkout main-dbt &&
-    git pull origin main-dbt
-    """,
-    dag=dag,
-)
-
-# dbt DAG configuration
 dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig("/usr/local/airflow/dags/dbt_testing"),
+    project_config=ProjectConfig("/usr/local/airflow/dags/dbt_testing",),
     operator_args={"install_deps": True},
     profile_config=profile_config,
-    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt"),
+    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
     schedule_interval="0 1 * * *",
     start_date=datetime(2024, 12, 25),
     catchup=False,
-    dag=dag,  # Pass the existing Airflow DAG to DbtDag
+    dag_id="dbt_dag",
 )
-
-# Task sequence: Pull the branch first, then run dbt
-pull_dbt_branch >> dbt_snowflake_dag
