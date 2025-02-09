a1ca8f7d8faaaf2d718a0a6f3792d4bd6bc1be6e
namasayaelvis <151112132+ElvisChanJunYu@users.noreply.github.com>
2025-02-10 03:44:33 +0800

    Update dbt_dag.py

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -15,7 +15,7 @@ profile_config = ProfileConfig(
 )
 
 dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig("/usr/local/airflow/dags/dbt_testing",),
+    project_config=ProjectConfig("/usr/local/airfl",),
     operator_args={"install_deps": True},
     profile_config=profile_config,
     execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
